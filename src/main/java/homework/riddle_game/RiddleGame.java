d57ef416527ac9498e7e658a8f9ce24200f17d03
AsiaKorushkina <nacta21011997@gmail.com>
2020-09-01 22:41:46 +0300

    added RiddleGame2

--- src/main/java/homework/riddle_game/RiddleGame.java
+++ src/main/java/homework/riddle_game/RiddleGame.java
@@ -1,8 +1,11 @@
 package homework.riddle_game;
 
 import design_patterns.template_method.Game;
+import design_patterns.template_method.Pair;
 
 import javax.swing.*;
+import java.util.List;
+import java.util.Random;
 
 /**
  * @author Evgeny Borisov
@@ -10,26 +13,49 @@ import javax.swing.*;
 public class RiddleGame implements Game {
 
     //todo
-
     private final int maxLimit;
+    private final RiddlePlayer player;
+    private final int num;
+    private int attempts;
 
-    public RiddleGame(int max) {
-        this.maxLimit = max;
-
+    public RiddleGame(int max, RiddlePlayer player) {
+        Random random = new Random();
+        maxLimit = max;
+        num = random.nextInt(max);
+        this.player = player;
     }
-
-    public static void main(String[] args) {
-
-        int s = Integer.parseInt(JOptionPane.showInputDialog("input your number"));
-        System.out.println(s);
-    }
-
-
 
 
 
     @Override
     public void play() {
+        boolean endOfGame = false;
+        while (!endOfGame || attempts > maxLimit) {
+            player.makeMove();
+            attempts++;
+            int ans = player.getAns();
+            if (ans == num){
+                double score = calculatedScore();
+                player.setScore(score);
+                if (score > player.getBest_score()) {
+                    player.setBest_score(score);
+                }
+                endOfGame = true;
+            }
+            else {
+                if (ans < num){
+                    System.out.println(ans + " is less than what the computer guessed. Try again!");
+                }
+                else {
+                    System.out.println(ans + " is larger than what the computer guessed. Try again!");
+                }
+            }
+        }
 
+    }
+
+    private double calculatedScore() {
+        double MAX_SCORE = 100.0;
+        return MAX_SCORE / attempts;
     }
 }
