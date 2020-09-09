e5ec0f20d296948e73afc65af07180052397633b
AsiaKorushkina <nacta21011997@gmail.com>
2020-09-09 22:36:56 +0300

    clean xml file

---
+++ src/main/java/real_spring/quoters/Conf.java
@@ -0,0 +1,9 @@
+package real_spring.quoters;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class Conf {
+}
