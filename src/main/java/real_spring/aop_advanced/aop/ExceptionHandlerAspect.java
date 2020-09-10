ecc5a62e32b3fad258c590a89ea7475928895c62
AsiaKorushkina <nacta21011997@gmail.com>
2020-09-10 18:28:46 +0300

    only repository

--- src/main/java/real_spring/aop_advanced/aop/ExceptionHandlerAspect.java
+++ src/main/java/real_spring/aop_advanced/aop/ExceptionHandlerAspect.java
@@ -3,9 +3,11 @@ package real_spring.aop_advanced.aop;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import real_spring.aop_advanced.services.DBRuntimeException;
 
 /**
@@ -18,7 +20,13 @@ public class ExceptionHandlerAspect {
 
 //    @Value()
 
-    @AfterThrowing(pointcut = "execution(* real_spring.aop_advanced.*.*(..))", throwing = "ex")
+    @Pointcut("@target(org.springframework.stereotype.Repository)")
+    public void repositoryMethods() {}
+
+    @Pointcut("execution(* real_spring.aop_advanced..*.*(..))")
+    public void allMethods() {}
+
+    @AfterThrowing(pointcut = "repositoryMethods() && allMethods()", throwing = "ex")
     public void handleDbException(DBRuntimeException ex) {
         System.out.println("sending mails "+ex.getMessage());
     }
