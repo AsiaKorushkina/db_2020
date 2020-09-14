a602fdd251195bc9cacc77b4ddff40aea5aebdf4
AsiaKorushkina <nacta21011997@gmail.com>
2020-09-14 10:51:39 +0300

    added Equalator

---
+++ src/main/java/java_8_examples/Equalator.java
@@ -0,0 +1,5 @@
+package java_8_examples;
+
+public interface Equalator<T> {
+    boolean equals(T t1, T t2);
+}
--- src/main/java/java_8_examples/ListUtils.java
+++ src/main/java/java_8_examples/ListUtils.java
@@ -6,10 +6,10 @@ import java.util.List;
  * @author Evgeny Borisov
  */
 public class ListUtils {
-    public static <T> int countDuplicates(List<T> list, T t) {
+    public static <T> int countDuplicates(List<T> list, T t, Equalator<T> equalator) {
         int counter=0;
         for (T t1 : list) {
-            if (t1.equals(t)) {
+            if (equalator.equals(t1, t)) {
                 counter++;
             }
         }
